be3483d8be377e6a7d0a75e55fa261f6da5f6f5d
Waqarghori <waqar.iqbal789@gmail.com>
2025-01-09 00:56:39 +0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+function calBtn(newChar) {
 
-function btnCalculater (char){
-let oo = document.getElementById("cal");
-oo.value = oo.value + char;
-console.log(char);
+    let cal = document.getElementById("inputField");
+    let lastChar = cal.value.slice(-1);
+    
+    if ((lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/")
+        && (newChar === "+" || newChar === "-" || newChar === "*" || newChar === "/")) {
+        cal.value = cal.value.slice(0, -1);
+        } else if (newChar == "=") {
+            cal.value = eval(cal.value);
+        } else {
+            cal.value += newChar;
+        }
 }
+
