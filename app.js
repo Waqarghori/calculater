ba34bd73afd5ab3168218fb555434be3364eed30
Waqarghori <waqar.iqbal789@gmail.com>
2025-03-08 17:11:04 +0500

    upd

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-function calBtn(newChar) {
 
-    let cal = document.getElementById("inputField");
-    let lastChar = cal.value.slice(-1);
-    
-    if ((lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/")
-        && (newChar === "+" || newChar === "-" || newChar === "*" || newChar === "/")) {
-        cal.value = cal.value.slice(0, -1);
-        } else if (newChar === "=") {
-            cal.value = eval(cal.value);
-        }else if (newChar === "C"){
-            cal.value = "";
-        } else {
-            cal.value += newChar;
-        }
+
+
+function calculate(value){
+
+
+    let input = document.getElementById("input")
+    let lastVal = input.value.slice(-1)
+
+    if((lastVal == "+" || lastVal == "-" || lastVal == "*" || lastVal == "/")
+        && (value == "+" || value == "-" || value == "*" || value == "/")){
+    input.value = input.value.slice(0, -1) + value
+    }else if(value == "="){
+        input.value = eval(input.value)
+    }else if(value == "bSpc"){
+        input.value = input.value.slice(0,-1)
+    }
+    else{
+        input.value += value
+    }
 }
+
+
+
+
+// function calBtn(newChar) {
+
+//     let cal = document.getElementById("inputField");
+//     let lastChar = cal.value.slice(-1);
+    
+//     if ((lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/")
+//         && (newChar === "+" || newChar === "-" || newChar === "*" || newChar === "/")) {
+//         cal.value = cal.value.slice(0, -1);
+//         } else if (newChar === "=") {
+//             cal.value = eval(cal.value);
+//         }else if (newChar === "C"){
+//             cal.value = "";
+//         } else {
+//             cal.value += newChar;
+//         }
+// }
+
 
